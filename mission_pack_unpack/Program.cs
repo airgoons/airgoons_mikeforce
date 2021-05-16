0ffa8d9b580de804ae9cba30f01bf1ef47385b85
wonkotron <wonkotron@gmail.com>
2021-05-16 13:35:13 -0700

    Initial commit for mission packaging utility

---
+++ mission_pack_unpack/Program.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace mission_pack_unpack {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
