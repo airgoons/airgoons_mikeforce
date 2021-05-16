ff4a869c70b706ed0b4b8e9d45ed33e9586f23a4
wonkotron <wonkotron@gmail.com>
2021-05-16 14:43:33 -0700

    Implement basic config file, rough-in functions

--- mission_pack_unpack/Program.cs
+++ mission_pack_unpack/Program.cs
@@ -1,9 +1,50 @@
 ﻿using System;
 
+using Config.Net;
+
 namespace mission_pack_unpack {
+
     class Program {
-        static void Main(string[] args) {
-            Console.WriteLine("Hello World!");
+        static int Main(string[] args) {
+
+            /*
+             * Mission Pack Unpack
+             * - Initialize values from configuration file
+             * - Downloads lateset official MikeForce PBO from Steam Workshop
+             * - Unpacks PBO using Arma 3 Tools
+             * - Copies AirGoons customizations
+             * - Packages PBO for manual deployment to server
+             */
+
+            try {
+                Configuration.InitSettings();
+                DownloadMikeForcePBO();
+                UnpackPBO();
+                CustomizeMission();
+                PackPBO();
+
+                return 0;
+            }
+            catch (Exception ex) {
+                Console.Error.WriteLine(ex.ToString());
+                return 1;
+            }
         }
-    }
+
+        static void DownloadMikeForcePBO() {
+            Console.Out.WriteLine("Downloading official Mike Force PBO");
+        }
+
+        static void UnpackPBO() {
+            Console.Out.WriteLine("Unpacking official Mike Force PBO");
+        }
+
+        static void CustomizeMission() {
+            Console.Out.WriteLine("Applying customizations");
+        }
+
+        static void PackPBO() {
+            Console.Out.WriteLine("Packing AirGoons Mike Force PBO");
+        }
+    }  
 }
