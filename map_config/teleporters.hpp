249a694b61de685af21e1280f571b106ca387df8
CookedAuto <cookedauto@gmail.com>
2025-08-18 22:06:31 +0200

    Added custom telporter function

---
+++ map_config/teleporters.hpp
@@ -0,0 +1,71 @@
+class objects
+{
+	class vn_mf_DeltaTroop_teleporter 
+	{
+		destinations[] = {"bravo_base", "alpha_base", "charlie_base", "delta_base", "farp_base", "navy_base"};
+	};
+
+	class vn_mf_BravoPlatoon_teleporter
+	{
+		destinations[] = {"bravo_base", "alpha_base", "charlie_base", "delta_base", "farp_base", "navy_base"};
+	};
+
+	class vn_mf_AlphaPlatoon_teleporter
+	{
+		destinations[] = {"bravo_base", "alpha_base", "charlie_base", "delta_base", "farp_base", "navy_base"};
+	};
+
+	class vn_mf_CharliePlatoon_teleporter
+	{
+		destinations[] = {"bravo_base", "alpha_base", "charlie_base", "delta_base", "farp_base", "navy_base"};
+	};
+	
+	class vn_mf_farp_teleporter
+	{
+		destinations[] = {"bravo_base", "alpha_base", "charlie_base", "delta_base", "farp_base", "navy_base"};
+	};
+	
+	class vn_mf_navy_teleporter
+	{
+		destinations[] = {"bravo_base", "alpha_base", "charlie_base", "delta_base", "farp_base", "navy_base"};
+	};
+};
+
+class destinations
+{
+	class bravo_base
+	{
+		image = "\vn\ui_f_vietnam\ui\wheelmenu\img\icons\vn_ico_mf_tel_spikebase_ca.paa";
+		position_marker = "mf_respawn_BravoPlatoon";
+	};
+
+	class alpha_base
+	{
+		image = "\vn\ui_f_vietnam\ui\wheelmenu\img\icons\vn_ico_mf_tel_mikebase_ca.paa";
+		position_marker = "mf_respawn_AlphaPlatoon";
+	};
+
+	class charlie_base
+	{
+		image = "\vn\ui_f_vietnam\ui\wheelmenu\img\icons\vn_ico_mf_tel_acavbase_ca.paa";
+		position_marker = "mf_respawn_CharliePlatoon";
+	};
+
+	class delta_base
+	{
+		image = "\vn\ui_f_vietnam\ui\wheelmenu\img\icons\vn_ico_mf_tel_airbase_ca.paa";
+		position_marker = "mf_respawn_DeltaTroop";
+	};
+	
+	class farp_base
+	{
+		image = "\vn\ui_f_vietnam\ui\wheelmenu\img\icons\vn_ico_mf_tel_airbase_ca.paa";
+		position_marker = "mf_respawn_farp";
+	};
+	
+	class navy_base
+	{
+		image = "\vn\ui_f_vietnam\ui\wheelmenu\img\icons\vn_ico_mf_tel_mikebase_ca.paa";
+		position_marker = "mf_respawn_navy";
+	};
+};
